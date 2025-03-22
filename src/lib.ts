25d329f79cce2a521e871e360a6d924391cfabae
jcg <jiang.chenguang@outlook.com>
2025-03-22 23:30:28 +0800

    feat: 添加button

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,2 @@
 import "./input.css"
-export { default as Link } from './component/link'
-export { default as httpService } from './decorator/http-service'
-export { default as HttpService } from './metadata/http-service'
+export { default as Button } from './component/button'
